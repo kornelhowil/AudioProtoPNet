476297eb42493d302fbdc7e6b4533ed9a6bb69d8
kornelhowil <kornelhowil@gmail.com>
2026-01-20 21:54:03 +0100

    Changes for evaluation

--- audioprotopnet/modules/baselines/convnext.py
+++ audioprotopnet/modules/baselines/convnext.py
@@ -43,22 +43,9 @@ class ConvNextClassifier(nn.Module):
         """
         super().__init__()
 
-        if pretrain_info:
-            self.hf_path = pretrain_info.hf_path
-            self.hf_name = (
-                pretrain_info.hf_name
-                if not pretrain_info.hf_pretrain_name
-                else pretrain_info.hf_pretrain_name
-            )
-            self.num_classes = len(
-                datasets.load_dataset_builder(self.hf_path, self.hf_name)
-                .info.features["ebird_code"]
-                .names
-            )
-        else:
-            self.hf_path = None
-            self.hf_name = None
-            self.num_classes = num_classes
+        self.hf_path = None
+        self.hf_name = None
+        self.num_classes = num_classes
 
         self.num_channels = num_channels
         self.embedding_size = embedding_size
